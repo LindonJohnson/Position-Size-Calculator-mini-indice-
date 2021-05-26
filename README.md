3049690a3c41dc117dd0eb4a60b326898109a3c2
lindonjhnson <lindonjhnson777@gmail.com>
2021-05-25 22:50:46 -0300

    version1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# electron-start-template
+## To Use
+
+```bash
+# Clone this repository
+
+# Go into the repository
+cd electron-quick-start
+# Install dependencies
+yarn install
+# Run the app
+yarn start
+```
+
+## To make a build
+
+```bash
+yarn build
+# The files will be in ./dist
+```
---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputRisco = event.target.querySelector("#risc");
+  const inputPontos = event.target.querySelector("#pts");
+
+  const risco = Number(inputRisco.value);
+  const pontos = Number(inputPontos.value);
+
+  if (!risco) {
+    set("ERRO", false);
+    return;
+  }
+
+  if (!pontos) {
+    set("ERRO", false);
+    return;
+  }
+
+  const ps = getPs(risco, pontos);
+
+  const msg = `${ps} C`;
+  set(msg, true);
+});
+function getnivelps() {
+  const nivel = [];
+}
+
+function getPs(risco, pontos) {
+  const ps = risco / (pontos * 0.2);
+  return ps.toFixed();
+}
+
+function pinnerhtml() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function set(msg, isValid) {
+  const result = document.querySelector("#resultado");
+
+  result.innerHTML = "";
+
+  const p = pinnerhtml();
+  p.innerHTML = msg;
+  result.appendChild(p);
+}
